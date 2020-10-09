1f05d63ac1b17b09dd73675fb78c0d19c98037fd
LucasFadavi91 <71818752+LucasFadavi91@users.noreply.github.com>
2020-10-09 18:46:40 +0200

    Add files via upload

---
+++ Bucles/ej6b.php
@@ -0,0 +1,19 @@
+<HTML>
+<HEAD><TITLE> EJ6B Factorial</TITLE></HEAD>
+<BODY>
+<h4>Lucas Fadavi Solanilla</h4>   
+<?php
+
+		$num="5";
+		$factorial = 1; 
+
+    for ($i = 1; $i <= $num; $i++){ 
+      $factorial = $factorial * $i; 
+
+    } 
+    	echo "El factorial de 5 es: " .$factorial;
+
+ 
+	?>
+</BODY>
+</HTML>
